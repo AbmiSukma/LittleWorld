5006b5dbb74d5d3ca2ff044cf9380c235441ca55
AbmiSukma <kurkurladpr@gmail.com>
2026-08-13 18:18:28 +0700

    Update HeroSection with new MountainLandscape structure

--- src/components/landscape/HeroSection.jsx
+++ src/components/landscape/HeroSection.jsx
@@ -21,49 +21,41 @@ const HeroSection = ({ timeState }) => {
 
   return (
     <section className={`hero-section ${timeState.id}`}>
-      {/* --- The New Unified Background --- */}
-      <div id="landscape-wrapper">
-        {/* Sky and Celestial Bodies */}
+      {/* --- New Mountain Landscape Background --- */}
+      <div className="landscape-wrapper">
+        {/* Sky and Clouds */}
         <div className="sky">
           <div className="moon"></div>
-          {/* Geometric Clouds */}
           <div className="cloud cloud-1"></div>
           <div className="cloud cloud-2"></div>
-          <div className="cloud cloud-3"></div>
         </div>
 
-        {/* Background Mountains */}
-        <div className="mountains">
-          <div className="mountain m-left"></div>
-          <div className="mountain m-center"></div>
-          <div className="mountain m-right"></div>
-          <div className="mountain m-small"></div>
+        {/* Layer Gunung - 5 Buah */}
+        <div className="mountain-layer">
+          <div className="mountain mt-1 mt-back"></div>
+          <div className="mountain mt-2 mt-back"></div>
+          <div className="mountain mt-3 mt-back"></div>
+          <div className="mountain mt-4 mt-mid"></div>
+          <div className="mountain mt-5 mt-mid"></div>
         </div>
 
-        {/* Foreground Hills */}
-        <div className="hills">
-          <div className="hill hill-left"></div>
-          <div className="hill hill-right"></div>
-          <div className="hill hill-center"></div>
+        {/* Layer Bukit - 3 Buah */}
+        <div className="hill-layer">
+          <div className="hill hill-1 hill-dark"></div>
+          <div className="hill hill-2 hill-dark"></div>
+          <div className="hill hill-3 hill-dark"></div>
         </div>
 
-        {/* Foreground Elements (Trees, Tent, Fire) */}
-        <div className="foreground">
-          {/* Wind lines */}
-          <div className="hero-wind-layer">
-            <svg viewBox="0 0 1440 600" preserveAspectRatio="none">
-               <path className="wind-line" d="M -200,200 Q 100,180 300,200 T 800,200 T 1600,200" fill="none" stroke="var(--fog)" strokeWidth="4" />
-               <path className="wind-line delay" d="M -200,300 Q 200,320 400,300 T 1000,300 T 1600,300" fill="none" stroke="var(--fog)" strokeWidth="3" />
-            </svg>
-          </div>
-
-          {/* Trees */}
+        {/* Campsite elements */}
+        <div className="campsite">
+          {/* Pohon - 5 Buah */}
           <div className="tree tree-1"></div>
           <div className="tree tree-2"></div>
           <div className="tree tree-3"></div>
           <div className="tree tree-4"></div>
+          <div className="tree tree-5"></div>
 
-          {/* Tent */}
+          {/* Tenda */}
           <div className="tent">
             <div className="tent-left"></div>
             <div className="tent-right"></div>
@@ -72,18 +64,23 @@ const HeroSection = ({ timeState }) => {
             <div className="tent-flag"></div>
           </div>
 
-          {/* Fireplace */}
-          <div className="fireplace">
-            <div className="log log-1"></div>
-            <div className="log log-2"></div>
-            <div className="fire fire-1"></div>
-            <div className="fire fire-2"></div>
-            <div className="fire fire-3"></div>
-            {/* Added Smoke Animation elements */}
+          {/* Api Unggun */}
+          <div className="campfire">
+            <div className="logs logs-1"></div>
+            <div className="logs logs-2"></div>
+            <div className="flame">
+              <div className="flame-inner"></div>
+            </div>
+            {/* Added Smoke Animation elements for consistency if wanted, otherwise can ignore */}
             <div className="smoke smoke-1"></div>
             <div className="smoke smoke-2"></div>
             <div className="smoke smoke-3"></div>
-            <div className="fire-glow"></div>
+          </div>
+
+          {/* UI Api Unggun (Burung) */}
+          <div className="birds">
+            <div className="bird bird-1"></div>
+            <div className="bird bird-2"></div>
           </div>
         </div>
       </div>
